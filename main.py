1a714588576f3232e4778280c4d6d92b36eac0ac
lepasq <pasquarelli.le@gmail.com>
2019-10-18 21:52:37 +0200

    added argument for repository name

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 import os
 import git
+import sys
 from github import Github
 
-path = "C:\\Users\\T440p\\Desktop\\Projects"
-newDir = "cmdline_will_be_added"
-newPath = path + "\\" + newDir
-repoName = "taskAutomation"
-userToken = "insert_your_token_here"
-userName = "insert_your_username_here"
-remoteUrl = "https://github.com/" + userName + "/" + repoName + ".git" 
+def main():
+    path = "C:\\Users\\T440p\\Desktop\\Projects"
+    # Line was replaced by argument from the commandline
+    #repoName = "repoName"
+    repoName = str(sys.argv[1])
+    newPath = path + "\\" + repoName
+    userToken = "token"
+    userName = "lepasq"
+    remoteUrl = "https://github.com/" + userName + "/" + repoName + ".git"
 
+    # Navigating to directory
+    os.chdir(path)
+    os.mkdir(repoName)
+    os.chdir(repoName)
+    os.system("touch README.md")
 
-# Navigating to directory
-os.chdir(path)
-os.mkdir(newDir)
-os.chdir(newDir)
-os.system("touch README.md")
+    # git init && git commit
+    r = git.Repo.init(newPath)
+    r.index.add("*")
+    r.index.commit("initial commit")
 
+    # Create repository on Github.com
+    g = Github(userToken)
+    user = g.get_user()
+    repo = user.create_repo(repoName)
 
-# git init && git commit
-r = git.Repo.init(newPath)
-r.index.add("*")
-r.index.commit("initial commit")
+    # Adding remote to git repository & pushing to Github
+    origin = r.create_remote('origin', remoteUrl)
+    r.remotes.origin.push(refspec='master:master')
 
+    # Open VSCode and open workspace
+    os.system("code .")
 
-# Create repository on Github.com
-g = Github(userToken)
-user = g.get_user()
-repo = user.create_repo(repoName)
-
-
-# Adding remote to git repository & pushing to Github
-origin = r.create_remote('origin', remoteUrl)
-r.remotes.origin.push(refspec='master:master')
-
-
-# Open VSCode and open workspace
-os.system("code .")
+main()
